5228485fde26734f119b934d893bd7287dfae387
AlfredoBaione <147424788+AlfredoBaione@users.noreply.github.com>
2025-02-21 16:28:31 +0100

    Update ellipsecons.m

--- ellipsecons.m
+++ ellipsecons.m
@@ -1,5 +1,5 @@
 function[c,ceq] = ellipsecons(x)
 ceq = [];
-%Controllo che i vincoli non lineari siano soddisfatti dalla soluzione
-%(sono soddisfatti se c<=0)
+%Check that the nonlinear constraints are satisfied by the solution
+%(they are satisfied if c<=0)
 c = -sin(4*pi*x(1))+2*(sin(2*pi*x(2)))^2-1.5;
